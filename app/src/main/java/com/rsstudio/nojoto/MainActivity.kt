bdf79c1f2063ebfd9b547f887964c5045f4b333c
Rishi Srivastava <official.rishisrivastava@example.com>
2022-09-27 21:45:42 +0530

    initial commit

---
+++ app/src/main/java/com/rsstudio/nojoto/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.rsstudio.nojoto
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.rsstudio.nojoto.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+    }
+
+}
